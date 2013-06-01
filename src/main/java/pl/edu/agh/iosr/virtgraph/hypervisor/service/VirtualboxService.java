4dd25c022e7e2d29815d89c1f834146f2ae743e7
Tomek <wawrotomasz@gmail.com>
2013-06-01 16:38:34 +0200

    Move connecting to the hypervisor to VirtualboxService class

--- src/main/java/pl/edu/agh/iosr/virtgraph/hypervisor/service/VirtualboxService.java
+++ src/main/java/pl/edu/agh/iosr/virtgraph/hypervisor/service/VirtualboxService.java
@@ -1,29 +1,53 @@
 package pl.edu.agh.iosr.virtgraph.hypervisor.service;
 
+import javax.annotation.PostConstruct;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.virtualbox_4_2.IVirtualBox;
+import org.virtualbox_4_2.VirtualBoxManager;
 
 import pl.edu.agh.iosr.virtgraph.hypervisor.communicator.ServerCommunicator;
-import pl.edu.agh.iosr.virtgraph.hypervisor.vmmanager.VirtualboxManagerWrapper;
 import pl.edu.agh.iosr.virtgraph.model.VirtualMachine;
+import pl.edu.agh.iosr.virtgraph.properties.Properties;
 
 @Service
 public class VirtualboxService implements VirtualMachineService {
 
-    private final static Logger LOGGER = LoggerFactory
-            .getLogger(VirtualboxService.class);
+	private final static Logger LOGGER = LoggerFactory
+			.getLogger(VirtualboxService.class);
 
-    @Autowired
-    VirtualboxManagerWrapper vBoxManager;
+	@Autowired
+	ServerCommunicator srvCommunicator;
 
-    @Autowired
-    ServerCommunicator srvCommunicator;
+	private VirtualBoxManager vBoxManager;
+	private IVirtualBox vBox = null;
 
-    @Override
-    public void start(VirtualMachine vm) {
-        LOGGER.debug("Starting a virtualmachine :" + vm);
-    }
+	@PostConstruct
+	public void init() {
+		vBoxManager = VirtualBoxManager.createInstance(null);
+		if (Properties.isConnectToVirtualboxOnStartup()) {
+			String user = Properties.getVirtualMachineUser();
+			String pass = Properties.getVirtualMachinePassword();
+			String address = Properties.getVirtualboxHypervisorAddress();
+			vBoxManager.connect(address, user, pass);
+			vBox = vBoxManager.getVBox();
+
+		}
+
+	}
+
+	@Override
+	public void start(VirtualMachine vm) {
+		LOGGER.debug("Starting a virtualmachine :" + vm);
+	}
+
+	@Override
+	public void startService(pl.edu.agh.iosr.virtgraph.model.Service service,
+			int virtualMachineID) {
+
+	}
 
 }
--- src/main/java/pl/edu/agh/iosr/virtgraph/hypervisor/vmmanager/VirtualboxManagerWrapper.java
+++ src/main/java/pl/edu/agh/iosr/virtgraph/hypervisor/vmmanager/VirtualboxManagerWrapper.java
@@ -2,36 +2,36 @@ package pl.edu.agh.iosr.virtgraph.hypervisor.vmmanager;
 
 import javax.annotation.PostConstruct;
 
-import org.springframework.stereotype.Component;
 import org.virtualbox_4_2.IVirtualBox;
 import org.virtualbox_4_2.VirtualBoxManager;
 
 import pl.edu.agh.iosr.virtgraph.properties.Properties;
 
-@Component
+//No longer used, to be removed once the new version works
+//@Component
 public class VirtualboxManagerWrapper {
 
-    // TODO: add appropriate logs if connection to hypervisor fails/is disabled
+	// TODO: add appropriate logs if connection to hypervisor fails/is disabled
 
-    private VirtualBoxManager vBoxManager;
-    private IVirtualBox vBox = null;
+	private VirtualBoxManager vBoxManager;
+	private IVirtualBox vBox = null;
 
-    public IVirtualBox getvBox() {
-        return vBox;
-    }
+	public IVirtualBox getvBox() {
+		return vBox;
+	}
 
-    @PostConstruct
-    public void init() {
-        vBoxManager = VirtualBoxManager.createInstance(null);
-        if (Properties.isConnectToVirtualboxOnStartup()) {
-            String user = Properties.getVirtualMachineUser();
-            String pass = Properties.getVirtualMachinePassword();
-            String address = Properties.getVirtualboxHypervisorAddress();
-            vBoxManager.connect(address, user, pass);
-            vBox = vBoxManager.getVBox();
+	@PostConstruct
+	public void init() {
+		vBoxManager = VirtualBoxManager.createInstance(null);
+		if (Properties.isConnectToVirtualboxOnStartup()) {
+			String user = Properties.getVirtualMachineUser();
+			String pass = Properties.getVirtualMachinePassword();
+			String address = Properties.getVirtualboxHypervisorAddress();
+			vBoxManager.connect(address, user, pass);
+			vBox = vBoxManager.getVBox();
 
-        }
+		}
 
-    }
+	}
 
 }
