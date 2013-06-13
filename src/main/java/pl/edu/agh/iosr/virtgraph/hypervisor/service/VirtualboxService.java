ada14da62a87e179f3f3ed936a00bb53bc1afdc9
tomek <wawrotomasz@gmail.com>
2013-06-14 00:52:00 +0200

    Start a service using a separate thread

--- src/main/java/pl/edu/agh/iosr/virtgraph/hypervisor/service/VirtualboxService.java
+++ src/main/java/pl/edu/agh/iosr/virtgraph/hypervisor/service/VirtualboxService.java
@@ -2,22 +2,27 @@ package pl.edu.agh.iosr.virtgraph.hypervisor.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Service;
 
-import pl.edu.agh.iosr.virtgraph.hypervisor.exception.NoSuchVMException;
-import pl.edu.agh.iosr.virtgraph.hypervisor.vmmanager.VirtualboxManagerWrapper;
+import pl.edu.agh.iosr.virtgraph.hypervisor.threads.ServiceRunnerThread;
 import pl.edu.agh.iosr.virtgraph.model.VirtualMachine;
 
 @Service
-public class VirtualboxService implements VirtualMachineService {
+public class VirtualboxService implements VirtualMachineService,
+        ApplicationContextAware {
 
     private final static Logger LOGGER = LoggerFactory
             .getLogger(VirtualboxService.class);
+    private ApplicationContext applicationContext = null;
 
-    @Autowired
-    private VirtualboxManagerWrapper vBWrapper;
+    /* riddiculous, but needs to be done this way for the spring autowiring to work */
 
+    /*
+     * @Autowired private VirtualboxManagerWrapper vBWrapper;
+     */
     @Override
     public void start(VirtualMachine vm) {
         // TODO implement me
@@ -27,16 +32,27 @@ public class VirtualboxService implements VirtualMachineService {
     @Override
     public void startService(String vmId,
             pl.edu.agh.iosr.virtgraph.model.Service service) {
-        try {
-            vBWrapper.startService(vmId, service);
-        } catch (NoSuchVMException e) {
-            e.printStackTrace();
-        }
-        // TODO update the state
+
+        // /FIXME: there must be a better way to do this!
+        // so, so ugly ;/
+        ServiceRunnerThread runner = applicationContext
+                .getBean(ServiceRunnerThread.class);
+        runner.setService(service);
+        runner.setVmId(vmId);
+
+        new Thread(runner).start();
     }
 
     @Override
     public void stopService(pl.edu.agh.iosr.virtgraph.model.Service service) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext ctx)
+            throws BeansException {
+        this.applicationContext = ctx;
         // TODO Auto-generated method stub
 
     }
---
+++ src/main/java/pl/edu/agh/iosr/virtgraph/hypervisor/threads/ServiceRunnerThread.java
@@ -0,0 +1,67 @@
+package pl.edu.agh.iosr.virtgraph.hypervisor.threads;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import pl.edu.agh.iosr.virtgraph.hypervisor.exception.NoSuchVMException;
+import pl.edu.agh.iosr.virtgraph.hypervisor.state.StateProvider;
+import pl.edu.agh.iosr.virtgraph.hypervisor.vmmanager.VirtualboxManagerWrapper;
+import pl.edu.agh.iosr.virtgraph.model.Service;
+
+/*Need to use spring to make autowiring work.*/
+@Component
+@Scope("prototype")
+public class ServiceRunnerThread implements Runnable {
+    private final static Logger LOGGER = LoggerFactory
+            .getLogger(ServiceRunnerThread.class);
+
+    @Autowired
+    VirtualboxManagerWrapper vBoxWrapper;
+
+    @Autowired
+    StateProvider stateProvider;
+
+    public String getVmId() {
+        return vmId;
+    }
+
+    public void setVmId(String vmId) {
+        this.vmId = vmId;
+    }
+
+    public Service getService() {
+        return service;
+    }
+
+    public void setService(Service service) {
+        this.service = service;
+    }
+
+    private String vmId;
+    private Service service;
+
+    public ServiceRunnerThread() {
+        //
+    }
+
+    public ServiceRunnerThread(String vmId, Service service) {
+        this.vmId = vmId;
+        this.service = service;
+    }
+
+    @Override
+    public void run() {
+        // TODO: implement me: run the service, notify the StateProvider
+        LOGGER.debug("Started a thread for running a service");
+        System.out.println("Service runner thread runs...");
+        try {
+            vBoxWrapper.startService(vmId, service);
+        } catch (NoSuchVMException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
