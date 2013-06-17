56a0322bcbede9cacc54cedcb3a80c5bd61232bd
Tomek <wawrotomasz@gmail.com>
2013-06-17 14:01:16 +0200

    Provide vm  and service registration
    On the database server

--- src/main/java/pl/edu/agh/iosr/virtgraph/hypervisor/communicator/RESTServerCommunicator.java
+++ src/main/java/pl/edu/agh/iosr/virtgraph/hypervisor/communicator/RESTServerCommunicator.java
@@ -26,73 +26,96 @@ import com.sun.jersey.api.client.config.DefaultClientConfig;
 @Component
 public class RESTServerCommunicator implements ServerCommunicator {
 
-    private final static Logger LOGGER = LoggerFactory
-            .getLogger(RESTServerCommunicator.class);
+	private final static Logger LOGGER = LoggerFactory
+			.getLogger(RESTServerCommunicator.class);
 
-    @Autowired
-    private StateProvider stateProvider;
+	@Autowired
+	private StateProvider stateProvider;
 
-    @Override
-    public String registerHost() throws NotRegisteredException {
-        /* TODO: add sensible properties to the Host instance */
-        Host host = new Host(Properties.getHostName(), Properties
-                .getSelfAddress());
+	@Override
+	public String registerHost() throws NotRegisteredException {
+		/* TODO: add sensible properties to the Host instance */
+		Host host = new Host(Properties.getHostName(),
+				Properties.getSelfAddress());
 
-        URI baseURI = UriBuilder.fromUri(Properties.getServerAddress()).build();
-        ClientConfig config = new DefaultClientConfig();
-        Client client = Client.create(config);
-        WebResource service = client.resource(baseURI);
-        ClientResponse response = service.path("hosts").type(
-                MediaType.APPLICATION_XML).entity(host).post(
-                ClientResponse.class);
-        if (response.getStatus() >= 300) {
-            LOGGER.debug("Failed to register on the server");
-            throw new NotRegisteredException();
-        } else {
-            LOGGER.debug("Successfully registered on the server. Response status code:"
-                    + response.getStatus());
-        }
-        return response.getLocation().toString();
-        /*
-         * if (Properties.isEnableVmRegistration()) { for (VirtualMachine vm : stateProvider.getVMs()) { registerVm(vm);
-         * } } else { LOGGER.debug("ENABLE_VM_REGISTRATION property is not set."); }
-         * 
-         * if (Properties.isEnableServiceRegistration()) { for (Service s : stateProvider.getServices()) {
-         * registerService(s); } } else { LOGGER.debug("ENABLE_SERVICE_REGISTRATION property is not set."); }
-         */
-        // FIXME prvide a way to register multipne services with one message
-        // (or at least one registerService() call)
+		URI baseURI = UriBuilder.fromUri(Properties.getServerAddress()).build();
+		ClientConfig config = new DefaultClientConfig();
+		Client client = Client.create(config);
+		WebResource service = client.resource(baseURI);
+		ClientResponse response = service.path("hosts")
+				.type(MediaType.APPLICATION_XML).entity(host)
+				.post(ClientResponse.class);
+		if (response.getStatus() >= 300) {
+			LOGGER.debug("Failed to register on the server");
+			throw new NotRegisteredException();
+		} else {
+			LOGGER.debug("Successfully registered on the server. Response status code:"
+					+ response.getStatus());
+		}
+		return response.getLocation().toString();
+		/*
+		 * if (Properties.isEnableVmRegistration()) { for (VirtualMachine vm :
+		 * stateProvider.getVMs()) { registerVm(vm); } } else {
+		 * LOGGER.debug("ENABLE_VM_REGISTRATION property is not set."); }
+		 * 
+		 * if (Properties.isEnableServiceRegistration()) { for (Service s :
+		 * stateProvider.getServices()) { registerService(s); } } else {
+		 * LOGGER.debug("ENABLE_SERVICE_REGISTRATION property is not set."); }
+		 */
+		// FIXME prvide a way to register multipne services with one message
+		// (or at least one registerService() call)
 
-        /*
-         * // Get plain text // Get plain text System.out.println(service.path("rest"
-         * ).path("hello").accept(MediaType.TEXT_PLAIN).get(String.class)); // Get XML
-         * System.out.println(service.path("rest").path("hello").accept(MediaType .TEXT_XML).get(String.class)); // The
-         * HTML System.out.println(service. path("rest").path("hello").accept(MediaType .TEXT_HTML).get(String.class));
-         */
-    }
+		/*
+		 * // Get plain text // Get plain text
+		 * System.out.println(service.path("rest"
+		 * ).path("hello").accept(MediaType.TEXT_PLAIN).get(String.class)); //
+		 * Get XML
+		 * System.out.println(service.path("rest").path("hello").accept(MediaType
+		 * .TEXT_XML).get(String.class)); // The HTML
+		 * System.out.println(service.
+		 * path("rest").path("hello").accept(MediaType
+		 * .TEXT_HTML).get(String.class));
+		 */
+	}
 
-    @Override
-    public void registerVm(VirtualMachine vm) throws NotRegisteredException {
-        // TODO
-    }
+	@Override
+	public void registerVm(VirtualMachine vm) throws NotRegisteredException {
+		URI baseURI = UriBuilder.fromUri(Properties.getServerAddress()).build();
+		ClientConfig config = new DefaultClientConfig();
+		Client client = Client.create(config);
+		WebResource webRes = client.resource(baseURI);
+		ClientResponse response = webRes.path("hosts")
+				.path(Properties.getHostName()).path("vms")
+				.type(MediaType.APPLICATION_XML).entity(vm)
+				.post(ClientResponse.class);
+		if (response.getStatus() >= 300) {
+			LOGGER.debug("Failed to register the vm on the server");
+			throw new NotRegisteredException();
+		} else {
+			LOGGER.debug("Successfully registered the vm on the server. Response status code:"
+					+ response.getStatus());
+		}
+	}
 
-    @Override
-    public void registerService(Service service) throws NotRegisteredException {
-        // TODO: add proper error messages in case of failure
-        URI baseURI = UriBuilder.fromUri(Properties.getServerAddress()).build();
-        ClientConfig config = new DefaultClientConfig();
-        Client client = Client.create(config);
-        WebResource webRes = client.resource(baseURI);
-        ClientResponse response = webRes.path("service").type(
-                MediaType.APPLICATION_XML).entity(service).post(
-                ClientResponse.class);
-        if (response.getStatus() >= 300) {
-            LOGGER.debug("Failed to register the service on the server");
-            throw new NotRegisteredException();
-        } else {
-            LOGGER.debug("Successfully registered the service on the server. Response status code:"
-                    + response.getStatus());
-        }
-    }
+	@Override
+	public void registerService(String vmid, Service service)
+			throws NotRegisteredException {
+		// TODO: add proper error messages in case of failure
+		URI baseURI = UriBuilder.fromUri(Properties.getServerAddress()).build();
+		ClientConfig config = new DefaultClientConfig();
+		Client client = Client.create(config);
+		WebResource webRes = client.resource(baseURI);
+		ClientResponse response = webRes.path("hosts")
+				.path(Properties.getHostName()).path("vms").path(vmid)
+				.path("services").type(MediaType.APPLICATION_XML)
+				.entity(service).post(ClientResponse.class);
+		if (response.getStatus() >= 300) {
+			LOGGER.debug("Failed to register the service on the server");
+			throw new NotRegisteredException();
+		} else {
+			LOGGER.debug("Successfully registered the service on the server. Response status code:"
+					+ response.getStatus());
+		}
+	}
 
 }
--- src/main/java/pl/edu/agh/iosr/virtgraph/hypervisor/state/StateProviderImpl.java
+++ src/main/java/pl/edu/agh/iosr/virtgraph/hypervisor/state/StateProviderImpl.java
@@ -21,84 +21,100 @@ import pl.edu.agh.iosr.virtgraph.properties.Properties;
 @Component
 public class StateProviderImpl implements StateProvider {
 
-    @Autowired
-    ServerCommunicator srvComm;
+	@Autowired
+	ServerCommunicator srvComm;
 
-    @Autowired
-    VirtualboxManagerWrapper vBoxManagerWrapper;
+	@Autowired
+	VirtualboxManagerWrapper vBoxManagerWrapper;
 
-    private String hostAddress = null;
+	private String hostAddress = null;
 
-    private final static Logger LOGGER = LoggerFactory
-            .getLogger(StateProviderImpl.class);
+	private final static Logger LOGGER = LoggerFactory
+			.getLogger(StateProviderImpl.class);
 
-    private Map<String, VirtualMachine> VMs;
+	private Map<String, VirtualMachine> VMs;
 
-    // FIXME - move initialization to one place (either here or ServerCommunicator)
-    @PostConstruct
-    public void init() throws NotRegisteredException {
-        if (Properties.isRegisterOnStartup()) {
-            hostAddress = srvComm.registerHost();
-            LOGGER.debug("HOST_ADDRESS is: " + hostAddress);
-        } else
-            LOGGER.debug("REGISTER_ON_STARTUP property is not set."
-                    + "Not registering a host");
+	// FIXME - move initialization to one place (either here or
+	// ServerCommunicator)
+	@PostConstruct
+	public void init() throws NotRegisteredException {
+		if (Properties.isRegisterOnStartup()) {
+			// FIXME: don't save the host address, use the knowledge about how
+			// it
+			hostAddress = srvComm.registerHost();
+			LOGGER.debug("HOST_ADDRESS is: " + hostAddress);
+		} else {
+			LOGGER.debug("REGISTER_ON_STARTUP property is not set."
+					+ "Not registering the host");
+		}
 
-        if (Properties.isConnectToVirtualboxOnStartup())
-            updateState();
-        else
-            LOGGER.debug("CONNECT_TO_VIRTUALBOX_ON_STARTUP property is not set."
-                    + "Not updating state");
-        VMs = vBoxManagerWrapper.getVMs();
+		if (Properties.isConnectToVirtualboxOnStartup()) {
+			updateState();
+		} else {
+			LOGGER.debug("CONNECT_TO_VIRTUALBOX_ON_STARTUP property is not set."
+					+ "Not updating state");
+		}
+		VMs = vBoxManagerWrapper.getVMs();
 
-    }
+		if (Properties.isEnableVmRegistration()) {
+			for (VirtualMachine vm : VMs.values()) {
+				srvComm.registerVm(vm);
+			}
 
-    @Override
-    public String getHostAddress() throws NotRegisteredException {
-        if (hostAddress == null)
-            throw new NotRegisteredException();
-        else
-            return hostAddress;
+		} else {
+			LOGGER.debug("ENABLE_VM_REGISTRATION property is not set."
+					+ "Not registering vms");
+		}
 
-    }
+	}
 
-    @Override
-    public List<Service> getServices() {
-        // TODO Auto-generated method stub
-        return null;
-    }
+	@Override
+	public String getHostAddress() throws NotRegisteredException {
+		if (hostAddress == null) {
+			throw new NotRegisteredException();
+		} else {
+			return hostAddress;
+		}
 
-    @Override
-    public Collection<VirtualMachine> getVMs() {
-        return VMs.values();
-    }
+	}
 
-    @Override
-    public VirtualMachine getVM(String id) {
-        return VMs.get(id);
-    }
+	@Override
+	public List<Service> getServices() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-    @Override
-    public void updateService(Service service) {
-        // TODO Auto-generated method stub
-        System.out.println("StateProvider received a service update");
-        updateState();
+	@Override
+	public Collection<VirtualMachine> getVMs() {
+		return VMs.values();
+	}
 
-    }
+	@Override
+	public VirtualMachine getVM(String id) {
+		return VMs.get(id);
+	}
 
-    @Override
-    public void updateState() {
-        // TODO Auto-generated method stub
-        LOGGER.debug("Update received, refreshing the state");
-        VMs = vBoxManagerWrapper.getVMs();
+	@Override
+	public void updateService(Service service) {
+		// TODO Auto-generated method stub
+		System.out.println("StateProvider received a service update");
+		updateState();
 
-    }
+	}
 
-    @Override
-    public void updateVM(VirtualMachine vm) {
-        // TODO Auto-generated method stub
-        System.out.println("StateProvider received a vm update");
-        updateState();
+	@Override
+	public void updateState() {
+		// TODO Auto-generated method stub
+		LOGGER.debug("Update received, refreshing the state");
+		VMs = vBoxManagerWrapper.getVMs();
 
-    }
+	}
+
+	@Override
+	public void updateVM(VirtualMachine vm) {
+		// TODO Auto-generated method stub
+		System.out.println("StateProvider received a vm update");
+		updateState();
+
+	}
 }
