23b6ed08bc9a96c3c33a9a0a1bde9c6e68d4a9ba
Tomek <wawrotomasz@gmail.com>
2013-06-22 16:27:19 +0200

    Provide a mock server to test security settings

--- src/main/java/pl/edu/agh/iosr/virtgraph/hypervisor/controller/TestController.java
+++ src/main/java/pl/edu/agh/iosr/virtgraph/hypervisor/controller/TestController.java
@@ -1,12 +1,19 @@
 package pl.edu.agh.iosr.virtgraph.hypervisor.controller;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.LinkedList;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
@@ -21,6 +28,9 @@ import pl.edu.agh.iosr.virtgraph.model.VirtualMachine;
 @Controller
 @Path("/test")
 public class TestController {
+
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(TestController.class);
 
 	@Autowired
 	StateProvider stateProvider;
@@ -58,7 +68,7 @@ public class TestController {
 	@GET
 	@Path("/hostlist")
 	@Produces("application/xml")
-	public HostList getHosts() {
+	public HostList getHostsX() {
 		LinkedList<Host> list = new LinkedList<Host>();
 		list.add(new Host("tomek-pc",
 				"http://localhost:8080/virtgraph/hypervisor/"));
@@ -88,6 +98,76 @@ public class TestController {
 		list.add(new Service("sshd", "/usr/bin/systemctl",
 				"/usr/bin/systemctl", false, startArgs, stopArgs));
 		return new ServiceList(list);
+	}
+
+	/* Mock the server */
+	@GET
+	@Path("/hosts/{hostName}/vms/{vmId}/services")
+	@Produces("application/xml")
+	public ServiceList getServices(@PathParam("hostName") String hostName,
+			@PathParam("vmId") String vmId) {
+
+		LOGGER.info("returning ServiceList for host:{}, and for VM: {}",
+				hostName, vmId);
+
+		LinkedList<Service> list = new LinkedList<Service>();
+		LinkedList<String> startArgs = new LinkedList<String>();
+		startArgs.add("start");
+		startArgs.add("sshd");
+		LinkedList<String> stopArgs = new LinkedList<String>();
+		stopArgs.add("stop");
+		stopArgs.add("sshd");
+		list.add(new Service("sshd", "/usr/bin/systemctl",
+				"/usr/bin/systemctl", false, startArgs, stopArgs));
+		return new ServiceList(list);
+	}
+
+	@GET
+	@Path("/hosts/{hostName}/vms")
+	@Produces("application/xml")
+	public VMList getVMs(@PathParam("hostName") String hostName) {
+		LOGGER.info("returning VMList for host:{}", hostName);
+		return new VMList(stateProvider.getVMs());
+	}
+
+	@GET
+	@Produces("application/xml")
+	@Path("/hosts")
+	public HostList getHosts() {
+		LOGGER.info("returning hostList");
+		LinkedList<Host> list = new LinkedList<Host>();
+		list.add(new Host("tomek-pc",
+				"http://localhost:8080/virtgraph/hypervisor/"));
+		HostList hlist = new HostList();
+		hlist.setHosts(list);
+		return hlist;
+	}
+
+	@POST
+	@Path("/hosts")
+	@Consumes("application/xml")
+	public Response registerHost(Host host) throws URISyntaxException {
+		LOGGER.info("registering host with name: {}", host.getName());
+		return Response.created(new URI("localhost")).build();
+	}
+
+	@POST
+	@Path("/hosts/{hostName}/vms")
+	@Consumes("application/xml")
+	public Response registerVM(@PathParam("hostName") String hostName,
+			VirtualMachine vm) throws URISyntaxException {
+		LOGGER.info("registering vms for host with name: {}", hostName);
+		return Response.created(new URI("localhost")).build();
+	}
+
+	@POST
+	@Path("/hosts/{hostName}/vms/{vmId}/services")
+	@Consumes("application/xml")
+	public Response registerService(@PathParam("hostName") String hostName,
+			@PathParam("vmId") String vmId, Service service)
+			throws URISyntaxException {
+		LOGGER.info("registering vms for host with name: {}", hostName);
+		return Response.created(new URI("localhost")).build();
 	}
 
 }
